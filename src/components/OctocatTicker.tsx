887dffc5a9f10d3982ca871fedc35c5715e51c7f
BensBar <45955857+BensBar@users.noreply.github.com>
2025-10-15 18:55:35 +0000

    Generated by Spark: same result

--- src/components/OctocatTicker.tsx
+++ src/components/OctocatTicker.tsx
@@ -14,84 +14,57 @@ interface OctocatTickerProps {
 }
 
 const FALLBACK_OCTOCATS: Octocat[] = [
-  { name: 'Original', image: 'https://octodex.github.com/images/original.png' },
-  { name: 'Stormtroopocat', image: 'https://octodex.github.com/images/stormtroopocat.png' },
-  { name: 'Hubot', image: 'https://octodex.github.com/images/hubot.jpg' },
-  { name: 'Octocat', image: 'https://octodex.github.com/images/octocat-de-los-muertos.jpg' },
-  { name: 'Daftpunktocat', image: 'https://octodex.github.com/images/daftpunktocat-guy.gif' },
-  { name: 'Riddlocat', image: 'https://octodex.github.com/images/riddlocat.png' },
-  { name: 'Megacat', image: 'https://octodex.github.com/images/megacat-2.png' },
-  { name: 'Filmtocat', image: 'https://octodex.github.com/images/filmtocat.png' },
-  { name: 'Privateinvestocat', image: 'https://octodex.github.com/images/privateinvestocat.jpg' },
-  { name: 'Dinotocat', image: 'https://octodex.github.com/images/dinotocat.png' },
-  { name: 'Octobiwan', image: 'https://octodex.github.com/images/octobiwan.jpg' },
-  { name: 'Gracehoppertocat', image: 'https://octodex.github.com/images/gracehoppertocat.jpg' },
-  { name: 'Droidtocat', image: 'https://octodex.github.com/images/droidtocat.png' },
-  { name: 'Spidertocat', image: 'https://octodex.github.com/images/spidertocat.png' },
-  { name: 'Steroidtocat', image: 'https://octodex.github.com/images/steroidtocat.png' },
-  { name: 'Scubatocat', image: 'https://octodex.github.com/images/scubatocat.png' },
-  { name: 'Shoptocat', image: 'https://octodex.github.com/images/shoptocat.png' },
-  { name: 'Manufacturetocat', image: 'https://octodex.github.com/images/manufacturetocat.png' },
-  { name: 'Poptocat', image: 'https://octodex.github.com/images/poptocat_v2.png' },
-  { name: 'Jetpacktocat', image: 'https://octodex.github.com/images/jetpacktocat.png' },
-  { name: 'Mountietocat', image: 'https://octodex.github.com/images/mountietocat.png' },
-  { name: 'Catstello', image: 'https://octodex.github.com/images/catstello.png' },
-  { name: 'Inspectocat', image: 'https://octodex.github.com/images/inspectocat.jpg' },
-  { name: 'Femalecodertocat', image: 'https://octodex.github.com/images/femalecodertocat.png' },
-  { name: 'Waldocat', image: 'https://octodex.github.com/images/waldocat.png' },
-  { name: 'Carlostocat', image: 'https://octodex.github.com/images/carlostocat.gif' },
-  { name: 'Collabocats', image: 'https://octodex.github.com/images/collabocats.jpg' },
-  { name: 'Okal-Eltocat', image: 'https://octodex.github.com/images/okal-eltocat.jpg' },
-  { name: 'Nyantocat', image: 'https://octodex.github.com/images/nyantocat.gif' },
-  { name: 'Codercat', image: 'https://octodex.github.com/images/codercat.jpg' },
-  { name: 'Kimonotocat', image: 'https://octodex.github.com/images/kimonotocat.png' },
-  { name: 'Droctocat', image: 'https://octodex.github.com/images/droctocat.png' },
-  { name: 'Plumber', image: 'https://octodex.github.com/images/plumber.jpg' },
-  { name: 'Vinyltocat', image: 'https://octodex.github.com/images/vinyltocat.png' },
-  { name: 'Electrocat', image: 'https://octodex.github.com/images/electrocat.png' },
-  { name: 'Defunktocat', image: 'https://octodex.github.com/images/defunktocat.png' },
-  { name: 'Labtocat', image: 'https://octodex.github.com/images/labtocat.png' },
-  { name: 'Surftocat', image: 'https://octodex.github.com/images/surftocat.png' },
-  { name: 'Welcometocat', image: 'https://octodex.github.com/images/welcometocat.png' },
-  { name: 'Cherryontop', image: 'https://octodex.github.com/images/cherryontop-o-cat.png' },
-  { name: 'Octonaut', image: 'https://octodex.github.com/images/octonaut.jpg' },
-  { name: 'Yaktocat', image: 'https://octodex.github.com/images/yaktocat.png' },
-  { name: 'Snowtocat', image: 'https://octodex.github.com/images/snowtocat_final.jpg' },
-  { name: 'Speedy', image: 'https://octodex.github.com/images/speedy-octopus.png' },
-  { name: 'Brennatocat', image: 'https://octodex.github.com/images/brennatocat.png' },
-  { name: 'FortyTwo', image: 'https://octodex.github.com/images/FortyTwo.jpeg' },
-  { name: 'Draculatocat', image: 'https://octodex.github.com/images/Dracula_v2.jpg' },
-  { name: 'Stevetocat', image: 'https://octodex.github.com/images/stevetocat.png' },
-  { name: 'Mona the Rivertocat', image: 'https://octodex.github.com/images/Professortocat_v2.png' },
-  { name: 'Twenty Percent Cooler', image: 'https://octodex.github.com/images/twenty-percent-cooler-octocat.png' },
-  { name: 'Minion', image: 'https://octodex.github.com/images/minion.png' },
-  { name: 'Sticktocat', image: 'https://octodex.github.com/images/sticktocat.png' },
-  { name: 'Baracktocat', image: 'https://octodex.github.com/images/baracktocat.jpg' },
-  { name: 'Heisencat', image: 'https://octodex.github.com/images/heisencat.png' },
-  { name: 'Saint Nictocat', image: 'https://octodex.github.com/images/saint_nictocat.jpg' },
-  { name: 'Collabocats', image: 'https://octodex.github.com/images/collabocats.jpg' },
-  { name: 'Dojocat', image: 'https://octodex.github.com/images/dojocat.jpg' },
-  { name: 'Benevocats', image: 'https://octodex.github.com/images/benevocats.png' },
-  { name: 'Luchador', image: 'https://octodex.github.com/images/luchadortocat.png' },
-  { name: 'Sentrytocat', image: 'https://octodex.github.com/images/Sentrytocat_octodex.jpg' },
-  { name: 'Umbrellatocat', image: 'https://octodex.github.com/images/umbrellatocat.png' },
-  { name: 'Homercat', image: 'https://octodex.github.com/images/homercat.png' },
-  { name: 'Mariachitacat', image: 'https://octodex.github.com/images/mariachitacat.jpg' },
-  { name: 'Octotron', image: 'https://octodex.github.com/images/octotron.jpg' },
-  { name: 'Dunetocat', image: 'https://octodex.github.com/images/dunetocat.png' },
-  { name: 'Skitchtocat', image: 'https://octodex.github.com/images/skitchtocat.png' },
-  { name: 'Linktocat', image: 'https://octodex.github.com/images/linktocat.jpg' },
-  { name: 'Mummytocat', image: 'https://octodex.github.com/images/mummytocat.gif' },
-  { name: 'Scarletteocat', image: 'https://octodex.github.com/images/scarletteocat.jpg' },
-  { name: 'Class Act', image: 'https://octodex.github.com/images/class-act.png' },
-  { name: 'Maxtocat', image: 'https://octodex.github.com/images/maxtocat.gif' },
-  { name: 'Bannekat', image: 'https://octodex.github.com/images/bannekat.png' },
-  { name: 'Pusheencat', image: 'https://octodex.github.com/images/Professortocat_v2.png' },
-  { name: 'Adventure Cat', image: 'https://octodex.github.com/images/adventure-cat.png' },
-  { name: 'Founding Father', image: 'https://octodex.github.com/images/founding-father.jpg' },
-  { name: 'Swagtocat', image: 'https://octodex.github.com/images/swagtocat.png' },
-  { name: 'Supportcat', image: 'https://octodex.github.com/images/supportcat.png' },
-  { name: 'Octoclark Kent', image: 'https://octodex.github.com/images/octoclark-kentocat.jpg' },
+  { name: 'github', image: 'https://avatars.githubusercontent.com/u/9919?s=200&v=4' },
+  { name: 'torvalds', image: 'https://avatars.githubusercontent.com/u/1024025?s=200&v=4' },
+  { name: 'gvanrossum', image: 'https://avatars.githubusercontent.com/u/2455448?s=200&v=4' },
+  { name: 'octocat', image: 'https://avatars.githubusercontent.com/u/583231?s=200&v=4' },
+  { name: 'tj', image: 'https://avatars.githubusercontent.com/u/25254?s=200&v=4' },
+  { name: 'yyx990803', image: 'https://avatars.githubusercontent.com/u/499550?s=200&v=4' },
+  { name: 'addyosmani', image: 'https://avatars.githubusercontent.com/u/110953?s=200&v=4' },
+  { name: 'sindresorhus', image: 'https://avatars.githubusercontent.com/u/170270?s=200&v=4' },
+  { name: 'kentcdodds', image: 'https://avatars.githubusercontent.com/u/1500684?s=200&v=4' },
+  { name: 'mojombo', image: 'https://avatars.githubusercontent.com/u/1?s=200&v=4' },
+  { name: 'defunkt', image: 'https://avatars.githubusercontent.com/u/2?s=200&v=4' },
+  { name: 'pjhyett', image: 'https://avatars.githubusercontent.com/u/3?s=200&v=4' },
+  { name: 'wycats', image: 'https://avatars.githubusercontent.com/u/4?s=200&v=4' },
+  { name: 'ezmobius', image: 'https://avatars.githubusercontent.com/u/5?s=200&v=4' },
+  { name: 'ivey', image: 'https://avatars.githubusercontent.com/u/6?s=200&v=4' },
+  { name: 'evanphx', image: 'https://avatars.githubusercontent.com/u/7?s=200&v=4' },
+  { name: 'vanpelt', image: 'https://avatars.githubusercontent.com/u/17?s=200&v=4' },
+  { name: 'wayneeseguin', image: 'https://avatars.githubusercontent.com/u/18?s=200&v=4' },
+  { name: 'brynary', image: 'https://avatars.githubusercontent.com/u/19?s=200&v=4' },
+  { name: 'kevinclark', image: 'https://avatars.githubusercontent.com/u/20?s=200&v=4' },
+  { name: 'technoweenie', image: 'https://avatars.githubusercontent.com/u/21?s=200&v=4' },
+  { name: 'macournoyer', image: 'https://avatars.githubusercontent.com/u/22?s=200&v=4' },
+  { name: 'takeo', image: 'https://avatars.githubusercontent.com/u/23?s=200&v=4' },
+  { name: 'caged', image: 'https://avatars.githubusercontent.com/u/25?s=200&v=4' },
+  { name: 'topfunky', image: 'https://avatars.githubusercontent.com/u/26?s=200&v=4' },
+  { name: 'anotherjesse', image: 'https://avatars.githubusercontent.com/u/27?s=200&v=4' },
+  { name: 'roland', image: 'https://avatars.githubusercontent.com/u/28?s=200&v=4' },
+  { name: 'lukas', image: 'https://avatars.githubusercontent.com/u/29?s=200&v=4' },
+  { name: 'fanvsfan', image: 'https://avatars.githubusercontent.com/u/30?s=200&v=4' },
+  { name: 'tomtt', image: 'https://avatars.githubusercontent.com/u/31?s=200&v=4' },
+  { name: 'railsjitsu', image: 'https://avatars.githubusercontent.com/u/32?s=200&v=4' },
+  { name: 'nitay', image: 'https://avatars.githubusercontent.com/u/34?s=200&v=4' },
+  { name: 'kevwil', image: 'https://avatars.githubusercontent.com/u/35?s=200&v=4' },
+  { name: 'KirinDave', image: 'https://avatars.githubusercontent.com/u/36?s=200&v=4' },
+  { name: 'jamesgolick', image: 'https://avatars.githubusercontent.com/u/37?s=200&v=4' },
+  { name: 'atmos', image: 'https://avatars.githubusercontent.com/u/38?s=200&v=4' },
+  { name: 'errfree', image: 'https://avatars.githubusercontent.com/u/44?s=200&v=4' },
+  { name: 'mojodna', image: 'https://avatars.githubusercontent.com/u/45?s=200&v=4' },
+  { name: 'bmizerany', image: 'https://avatars.githubusercontent.com/u/46?s=200&v=4' },
+  { name: 'jnewland', image: 'https://avatars.githubusercontent.com/u/47?s=200&v=4' },
+  { name: 'joshknowles', image: 'https://avatars.githubusercontent.com/u/48?s=200&v=4' },
+  { name: 'hornbeck', image: 'https://avatars.githubusercontent.com/u/49?s=200&v=4' },
+  { name: 'jwhitmire', image: 'https://avatars.githubusercontent.com/u/50?s=200&v=4' },
+  { name: 'elbowdonkey', image: 'https://avatars.githubusercontent.com/u/51?s=200&v=4' },
+  { name: 'timtrueman', image: 'https://avatars.githubusercontent.com/u/52?s=200&v=4' },
+  { name: 'bleything', image: 'https://avatars.githubusercontent.com/u/54?s=200&v=4' },
+  { name: 'jnunemaker', image: 'https://avatars.githubusercontent.com/u/235?s=200&v=4' },
+  { name: 'josh', image: 'https://avatars.githubusercontent.com/u/137?s=200&v=4' },
+  { name: 'rails', image: 'https://avatars.githubusercontent.com/u/4223?s=200&v=4' },
+  { name: 'facebook', image: 'https://avatars.githubusercontent.com/u/69631?s=200&v=4' },
+  { name: 'google', image: 'https://avatars.githubusercontent.com/u/1342004?s=200&v=4' },
 ]
 
 export function OctocatTicker({ height = 64, padding, speed = 60 }: OctocatTickerProps) {
@@ -99,24 +72,8 @@ export function OctocatTicker({ height = 64, padding, speed = 60 }: OctocatTicke
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    async function fetchOctocats() {
-      try {
-        const response = await fetch('https://api.github.com/octocat')
-        
-        if (!response.ok) {
-          throw new Error('Failed to fetch')
-        }
-
-        setOctocats(FALLBACK_OCTOCATS)
-      } catch (error) {
-        console.warn('Using fallback Octocats:', error)
-        setOctocats(FALLBACK_OCTOCATS)
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    fetchOctocats()
+    setOctocats(FALLBACK_OCTOCATS)
+    setLoading(false)
   }, [])
 
   const displayOctocats = [...octocats, ...octocats]
